49bbd90ea2160efecbeca74c6242d8c01a4df92f
Eric Kreutzer <eric@lu.gg>
2015-11-09 12:13:07 -0800

    Updates

--- Gemfile
+++ Gemfile
@@ -1,9 +1,12 @@
 # If you have OpenSSL installed, we recommend updating
 # the following line to use "https"
-source 'http://rubygems.org'
+source 'https://rubygems.org'
 
-gem 'middleman', '~> 3.3.6'
-gem 'middleman-livereload', '~> 3.3.4'
-gem 'middleman-autoprefixer'
-gem 'middleman-sync'
-gem 'dotenv', '~> 1.0.1'
+gem "middleman", "~> 3.4.0"
+gem "middleman-livereload"
+gem "middleman-autoprefixer"
+gem "middleman-minify-html"
+gem "middleman-s3_sync"
+gem "middleman-dotenv", "~> 1.1.1"
+gem "middleman-imageoptim", github: "plasticine/middleman-imageoptim"
+gem "actionview", "~> 4.1.9"
--- config.rb
+++ config.rb
@@ -1,46 +1,72 @@
-require 'dotenv'
+require "dotenv"
+require "active_support/all"
+require "action_view"
 
-Dotenv.load
+#-----------------------------------------------------------------------------
+# Directories
+#-----------------------------------------------------------------------------
 
-set :css_dir, 'stylesheets'
-set :js_dir, 'javascripts'
-set :images_dir, 'images'
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
+
+#-----------------------------------------------------------------------------
+# Plugins
+#-----------------------------------------------------------------------------
 
 activate :livereload
-activate :autoprefixer do |config|
-  config.browsers = ['last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4']
+activate :dotenv
+activate :directory_indexes
+
+activate :imageoptim do |config|
+  config.manifest = true
+  config.image_extensions = %w(.png .jpg .gif .svg)
+  config.pngout = false
+  config.svgo = false
 end
+
+activate :autoprefixer do |config|
+  config.browsers = ["last 2 version", "safari 5", "ie 8", "ie 9", "opera 12.1", "ios 6", "android 4"]
+end
+
+activate :s3_sync do |sync|
+  sync.bucket = ENV.fetch("AWS_BUCKET_NAME")
+  sync.region = ENV.fetch("AWS_REGION")
+  sync.aws_access_key_id = ENV.fetch("AWS_ACCESS_KEY_ID")
+  sync.aws_secret_access_key = ENV.fetch("AWS_SECRET_ACCESS_KEY")
+  sync.delete = true
+  sync.path_style = true
+  sync.prefer_gzip = true
+  sync.after_build = false
+end
+
+#-----------------------------------------------------------------------------
+# Misc Config
+#-----------------------------------------------------------------------------
 
 after_configuration do
   @bower_config = JSON.parse(IO.read("#{root}/.bowerrc"))
-  sprockets.append_path File.join "#{root}", @bower_config['directory']
-end
-
-activate :sync do |sync|
-  sync.fog_provider = 'AWS'
-  sync.fog_directory = ENV['AWS_BUCKET_NAME']
-  sync.fog_region = 'us-east-1'
-  sync.aws_access_key_id = ENV['AWS_ACCESS_KEY_ID']
-  sync.aws_secret_access_key = ENV['AWS_SECRET_ACCESS_KEY']
-  sync.existing_remote_files = 'keep'
-  sync.gzip_compression = true
-  sync.after_build = false
+  sprockets.append_path File.join "#{root}", @bower_config["directory"]
 end
 
 # Build-specific configuration
 configure :build do
   activate :gzip
-
-  ignore 'bower/**/*'
-  ignore 'images/icons/*'
-  ignore 'images/icons-2x/*'
-  ignore 'stylesheets/app/**/*'
-  ignore 'stylesheets/libs/**/*'
-  ignore 'stylesheets/vendor/**/*'
-  ignore 'javascripts/vendor/**/*'
-  ignore 'javascripts/libs/**/*'
-  ignore 'javascripts/app/**/*'
-
+  activate :asset_hash
   activate :minify_css
   activate :minify_javascript
+  activate :minify_html
+
+  ignore "images/icons/*"
+  ignore "images/icons-2x/*"
+  ignore "stylesheets/app/*"
+  ignore "javascripts/app/*"
+end
+
+#-----------------------------------------------------------------------------
+# Helpers
+#-----------------------------------------------------------------------------
+
+helpers do
+  # def helper_method; end
 end
